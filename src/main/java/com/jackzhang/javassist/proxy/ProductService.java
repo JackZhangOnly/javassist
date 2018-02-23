0c20b7b5edace2049f513e621e2458f5cbfd6210
JackZhang <1021938637@qq.com>
2018-02-23 19:40:36 +0800

    proxy

---
+++ src/main/java/com/jackzhang/javassist/proxy/ProductService.java
@@ -0,0 +1,13 @@
+package com.jackzhang.javassist.proxy;
+
+/**
+ * Created by Jack
+ */
+public class ProductService {
+    public void save(){
+        System.out.println("保存商品");
+    }
+    public void del(){
+        System.out.println("删除商品");
+    }
+}
